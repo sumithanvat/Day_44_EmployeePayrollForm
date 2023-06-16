de03ba51abd86e787c2daf1594b83c847f495cc8
Sumithanvat <skhanvat@gmail.com>
2023-06-17 00:11:29 +0530

    Added: Set Event Listner

--- Employee.js
+++ Employee.js
@@ -54,3 +54,27 @@ class EmployeePayrollData{
     return "id= "+this.id+" name= "+this.name + " gender= "+this.gender+" profilePic= "+this.profilePic+" department= "+this.department+" salary= "+this.salary +" startDate " +empDate+" note " +this.note;
    }
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name'); 
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) { 
+            textError.textContent ="";
+            return;
+        }    
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+             textError.textContent = e;
+        }
+    
+    });
+    const salary=document.querySelector('#salary');
+    const output=document.querySelector('.salary-output'); 
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+    output.textContent=salary.value;
+    });
+});
